6111c7652cdf4e09df56e2641a90fdca3c6197cb
xiebaiyuan <xiebaiyuan@baidu.com>
2024-05-08 23:02:52 +0800

    add kill.sh

--- kill.sh
+++ kill.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# 获取所有 npm start 相关进程的 PID
-pids=$(ps aux | grep 'npm start' | grep -v grep | awk '{print $2}')
+# 获取所有 npm start 和 node 相关进程的 PID
+pids=$(ps aux | grep -E 'npm start|node' | grep -v grep | awk '{print $2}')
 
 # 遍历每个 PID 并杀死相关进程
 for pid in $pids; do
@@ -8,4 +8,4 @@ for pid in $pids; do
     kill -9 $pid
 done
 
-echo "All npm start processes have been killed."
+echo "All npm start and node processes have been killed."
